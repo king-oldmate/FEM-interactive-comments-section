5d3ef544508a3982f61f5d9d3378633150d28add
king-oldmate <amachizzle@gmail.com>
2022-07-16 10:52:31 +1000

    started on the last component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Post from "./Post";
+import SendIt from "./SendIt";
 import data from "./data.json";
 function App() {
   const { currentUser, comments } = data;
   const { image, username } = currentUser;
   const currentUserImage = image.png;
   return (
-    <div className='max-w-[732px] mx-auto'>
+    <main className='max-w-[732px] mx-auto'>
       {comments.map((comment) => {
         return (
           <Post
@@ -16,7 +17,8 @@ function App() {
           />
         );
       })}
-    </div>
+      <SendIt />
+    </main>
   );
 }
 
---
+++ src/SendIt.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SendIt = () => {
+  return <div>SendIt</div>;
+};
+
+export default SendIt;
